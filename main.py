b97b5f159af41798a4e822ca477cc759a60d9511
Kethankrk <kethankrk@gmail.com>
2023-06-10 10:25:52 +0530

    added virtual env

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from PIL import Image
 
 app = Flask(__name__)
 
+
+
+@app.route("/topng")
+def toPng():
+    try:
+        image = Image.open("./input.jpg")
+        image.save("output.png", "PNG")
+        return "success"
+    except Exception as e:
+        return f"This is the error {e.args}"
+
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
