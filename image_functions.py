7c4f00f3968109bc93c7bcb31057c03a5f7d2084
Shahadil Munawir <shahadilmunawir110@gmail.com>
2023-06-10 11:03:41 +0530

    add function: toPng

---
+++ image_functions.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+def toPng():
+    try:
+        image = Image.open("./input.jpg")
+        image.save("output.png", "PNG")
+        return "success"
+    except Exception as e:
+        return f"This is the Error: {e.args}"
