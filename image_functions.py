5beff5b60f451bc5548c40667f0f7f378520e7d3
Kethankrk <kethankrk@gmail.com>
2023-06-10 12:10:46 +0530

    added a new parameter and made the function iterate through the give list and save the file with an incrementing number at the end

--- image_functions.py
+++ image_functions.py
@@ -1,10 +1,11 @@
 from PIL import Image
 
 
-def toPng():
+def toPng(files):
     try:
-        image = Image.open("./input.jpg")
-        image.save("output.png", "PNG")
+        for i, file in enumerate(files):
+            image = Image.open(file)
+            image.save(f"./outputImages/output{i}.png", "PNG")
         return "success"
     except Exception as e:
         return f"This is the Error: {e.args}"
