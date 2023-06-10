f144c23be6f6f5f22cd9476f8e50ba4ca4ee1343
Kethankrk <kethankrk@gmail.com>
2023-06-10 13:49:10 +0530

    added option to define output file quality and extension

--- image_functions.py
+++ image_functions.py
@@ -1,11 +1,11 @@
 from PIL import Image
 
 
-def toPng(files):
+def toPng(files, extension, quality):
     try:
         for i, file in enumerate(files):
             image = Image.open(file)
-            image.save(f"./outputImages/output{i}.png", "PNG")
+            image.save(f"./outputImages/output{i}.{extension}", f"{extension}", quality=quality)
         return "success"
     except Exception as e:
         return f"This is the Error: {e.args}"
