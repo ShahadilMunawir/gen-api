f2f10dca64f09166d01b3c611cb04bee690c1e7f
Kethankrk <kethankrk@gmail.com>
2023-06-11 19:36:39 +0530

    added the auto cleanup function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PIL import Image
 from flask import Flask, request, send_file
 from image_functions import toPng, downloadFunction, backgroundImageRemove
+import threading
+import os
+import time
 
 app = Flask(__name__)
 
@@ -48,5 +51,30 @@ def bg_remove():
     
     image = request.files.getlist("images")
     return backgroundImageRemove(image)
+
+
+def auto_delete():
+    print("running the auto cleanup function")
+    try:
+        current_time = time.time()
+        for file_name in os.listdir('./outputImages'):
+            file_path = os.path.join("./outputImages", file_name)
+            if current_time - os.path.getctime(file_path) > 300:
+                os.remove(file_path)
+                print(f"removed {file_path} successfully")
+    except Exception as e:
+        print(f"An error occured while auto deleting the files")
+
+def cleanup():
+    while True:
+        auto_delete()
+        time.sleep(300)
+
+cleanup_thread = threading.Thread(target=cleanup)
+cleanup_thread.daemon = True
+cleanup_thread.start()
+
+
+auto_delete()
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
