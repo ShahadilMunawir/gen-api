fb73ac03b232625f8189c2dbbb1ecd6d1ba33484
Kethankrk <kethankrk@gmail.com>
2023-06-11 11:54:09 +0530

    toPng function now returns a list of download links

--- image_functions.py
+++ image_functions.py
@@ -1,11 +1,22 @@
 from PIL import Image
+import os
 
 
 def toPng(files, extension, quality):
     try:
+        download_file_list = []
         for i, file in enumerate(files):
             image = Image.open(file)
             image.save(f"./outputImages/output{i}.{extension}", f"{extension}", quality=quality)
-        return "success"
+            download_file_list.append(f"http://localhost:5000/download/output{i}.{extension}")
+        return download_file_list
     except Exception as e:
         return f"This is the Error: {e.args}"
+
+
+def downloadFunction(filename):
+    try:
+        file_path = os.path.join("./outputImages", filename)
+        return file_path
+    except Exception:
+        return "File not found"
